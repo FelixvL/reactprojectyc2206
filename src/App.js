d6c3c855745264ec539eaa8d7bbf02a2f3325a35
FelixvL <digitaleservice@gmail.com>
2022-06-28 19:28:24 +0200

    call op backend werkend data json in log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+const api = axios.create({
+  baseURL: `http://localhost:8082/felixallemaaltijden`
+})
+
+
+
+class App extends Component {
+  constructor(){
+    super();
+    api.get('/').then( res => {
+      console.log(res.data);
+    })
+  }
+
+  render() {
+    return (<div className="App">
       hoi
     </div>
-  );
+    )
+  }
 }
 
 export default App;
