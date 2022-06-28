b00f347398189b5691ba70f2021fd23e78a15652
FelixvL <digitaleservice@gmail.com>
2022-06-28 20:58:14 +0200

    post werkt - updaten dom/state werkt - didcomponentmount werkt

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 //import logo from './logo.svg';
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:8082/felixallemaaltijden`
+  baseURL: `http://localhost:8082/`
 })
 
 
 
 class App extends Component {
-  state = {
-    maaltijden:[]
-  }
 
-  constructor(){
-    super();
-    api.get('/').then( res => {
+
+  
+
+  createMaaltijd = async() =>{
+    let res = await api.post('felixmaakmaaltijdaan',{naam:"test",id:9})
+    console.log(res.data.naam);
+    this.setState({ naamMaaltijd : res.data.naam})
+    
+  }
+  constructor(props){
+    super(props);
+    this.state = {
+      maaltijden:[],
+      naamMaaltijd : "a"
+    }
+  }
+  componentDidMount() {
+    api.get('/felixallemaaltijden').then( res => {
       console.log(res.data);
       this.setState({ maaltijden : res.data})
+      
     })
   }
 
   render() {
     return (<div className="App">
+      {this.state.naamMaaltijd}
+      <button onClick={this.createMaaltijd}>create Maaltijd</button>
       {this.state.maaltijden.map( maaltijd => <h1 key={maaltijd.id}>{maaltijd.naam}</h1>)}      
     </div>
     )
